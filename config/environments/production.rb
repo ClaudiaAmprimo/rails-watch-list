79f7b4a7a2ee60e1428334c536f5f735ac4690b7
claudiaamprimo <claudia.amprimo@gmail.com>
2023-10-02 12:12:51 +0200

    working on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,14 +7,10 @@ Rails.application.configure do
   config.cache_classes = true
 
   config.active_storage.service = :cloudinary
-  config.active_storage.service_config = {
-    cloudinary: {
-      service: 'Cloudinary',
-      cloud_name: 'dz4ibgzwj',    # Replace with your Cloudinary cloud name
-      api_key: '499925347483352',          # Replace with your Cloudinary API key
-      api_secret: ENV['SFrq1x8Z1PzS0ejV_MgIkTNdF20']  # Access API secret from environment variable
-    }
-  }
+  config.cloudinary.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+  config.cloudinary.api_key = ENV['CLOUDINARY_API_KEY']
+  config.cloudinary.api_secret = ENV['CLOUDINARY_API_SECRET']
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
